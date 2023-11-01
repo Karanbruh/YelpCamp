75eec6c7cf9100404ed9e397539289058fc7b415
Karanbruh <115772316+Karanbruh@users.noreply.github.com>
2023-11-01 18:41:15 +0530

    Update index.js

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
-const cities = require('./cities');
+const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -12,38 +12,41 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
+
 const seedDB = async () => {
     await Campground.deleteMany({});
+    const citiesCount = cities.length
     for (let i = 0; i < 300; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+        const randomIndex = Math.floor(Math.random() * citiesCount);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author: '64dcb82f1c031082d816cf77',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            location: `${cities[randomIndex].City}, ${cities[randomIndex].State}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
             geometry: {
                 type: "Point",
                 coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude,
+                    cities[randomIndex].Longitude,
+                    cities[randomIndex].Latitude,
                 ]
             },
-            images:  [
+            images: [
                 {
-                  url: 'https://res.cloudinary.com/dy0g6td7t/image/upload/v1692638255/YelpCamp/xlgamqyjdwhv8rwguti6.jpg',
-                  filename: 'YelpCamp/xlgamqyjdwhv8rwguti6'
+                    url: 'https://res.cloudinary.com/dy0g6td7t/image/upload/v1692638255/YelpCamp/xlgamqyjdwhv8rwguti6.jpg',
+                    filename: 'YelpCamp/xlgamqyjdwhv8rwguti6'
                 },
                 {
-                  url: 'https://res.cloudinary.com/dy0g6td7t/image/upload/v1692638347/YelpCamp/t7kkf2isejfe4rc5kfxj.jpg',
-                  filename: 'YelpCamp/t7kkf2isejfe4rc5kfxj'
+                    url: 'https://res.cloudinary.com/dy0g6td7t/image/upload/v1692638347/YelpCamp/t7kkf2isejfe4rc5kfxj.jpg',
+                    filename: 'YelpCamp/t7kkf2isejfe4rc5kfxj'
                 }
-              ]
+            ]
         })
         await camp.save();
     }
 }
+
 
 seedDB().then(() => {
     mongoose.connection.close();
